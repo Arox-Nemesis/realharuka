53a8521bdefe40bd0c14daf23696c230cf39ad30
Arox-Nemesis <135242263+Arox-Nemesis@users.noreply.github.com>
2026-06-23 11:36:11 +0530

    Update bot

--- bot.py
+++ bot.py
@@ -52,12 +52,12 @@ class Bot(Client):
             sys.exit()
 
         self.set_parse_mode(ParseMode.HTML)
-        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/CodeflixSupport")
-        self.LOGGER(__name__).info(f"""BOT DEPLOYED BY @CODEFLIX_BOTS""")
+        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/ongoing_swordsmith")
+        self.LOGGER(__name__).info(f"""BOT DEPLOYED BY ANIME SWORDSMITH""")
 
         self.set_parse_mode(ParseMode.HTML)
         self.username = usr_bot_me.username
-        self.LOGGER(__name__).info(f"Bot Running..! Made by @Codeflix_Bots")   
+        self.LOGGER(__name__).info(f"Bot Running..! Yay 🎉")   
 
         # Start Web Server
         app = web.AppRunner(await web_server())
@@ -65,7 +65,7 @@ class Bot(Client):
         await web.TCPSite(app, "0.0.0.0", PORT).start()
 
 
-        try: await self.send_message(OWNER_ID, text = f"<b><blockquote> Bᴏᴛ Rᴇsᴛᴀʀᴛᴇᴅ by @Codeflix_Bots</blockquote></b>")
+        try: await self.send_message(OWNER_ID, text = f"<b><blockquote> Bᴏᴛ Rᴇsᴛᴀʀᴛᴇᴅ !!</blockquote></b>")
         except: pass
 
     async def stop(self, *args):
@@ -76,7 +76,7 @@ class Bot(Client):
         """Run the bot."""
         loop = asyncio.get_event_loop()
         loop.run_until_complete(self.start())
-        self.LOGGER(__name__).info("Bot is now running. Thanks to @rohit_1888")
+        self.LOGGER(__name__).info("Bot is now running")
         try:
             loop.run_forever()
         except KeyboardInterrupt:
